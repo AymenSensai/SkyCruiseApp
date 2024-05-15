ef32d55fc78da857c22a7ab2b6c6822b420e9a00
Aymen <bestaouiaymene@gmail.com>
2024-05-15 16:48:21 +0100

    - Added Security screen.

--- lib/core/theming/colors.dart
+++ lib/core/theming/colors.dart
@@ -27,6 +27,7 @@ class ColorsManager {
   static const Color neutral500 = Color(0xff757575);
   static const Color neutral600 = Color(0xff6A6A6A);
   static const Color neutral700 = Color(0xff535353);
+  static const Color neutral800 = Color(0xff404040);
   static const Color neutral900 = Color(0xff242424);
 
   static const Color whiteBackground = Color(0xffffffff);
---
+++ lib/features/profile/presentation/screens/security.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/helpers/spacing.dart';
+import '../../../../core/theming/colors.dart';
+import '../../../../core/theming/styles.dart';
+import '../../../../core/widgets/app_text_button.dart';
+
+class SecurityScreen extends StatelessWidget {
+  const SecurityScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Security'),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(left: 24.w, right: 24.w, top: 12.h),
+            child: Column(
+              children: [
+                _securityRow(
+                    'Password',
+                    'Reset your password regularly to keep your account secure',
+                    () {},
+                    screenWidth),
+                _securityRow(
+                    'Two-factor authentication',
+                    'Add a phone number to set up two-factor authentication',
+                    () {},
+                    screenWidth),
+                _securityRow(
+                    'Active sessions',
+                    'Sign out from all the active sessions',
+                    () {},
+                    screenWidth),
+                verticalSpace(20),
+                _deleteAccountButton()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _securityRow(String title, String description, VoidCallback onTap,
+      double screenWidth) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 12.h),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: TextStyles.font16Neutral900Medium,
+              ),
+              const Icon(
+                Icons.arrow_forward_ios_rounded,
+                size: 18,
+                color: ColorsManager.neutral900,
+              )
+            ],
+          ),
+          verticalSpace(8),
+          SizedBox(
+            width: screenWidth - 100.w,
+            child: Text(
+              description,
+              style: TextStyles.font14Neutral800Regular,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _deleteAccountButton() {
+    return AppTextButton(
+      buttonText: 'Delete Account',
+      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+          side: const BorderSide(color: ColorsManager.error500, width: 1.0),
+        ),
+      ),
+      backgroundColor: Colors.transparent,
+      textStyle: TextStyles.font16Error500Semibold,
+      onPressed: () {},
+    );
+  }
+}
